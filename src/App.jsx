20767d75bfbc676facb3bc0e72b112d1a6a7a56d
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-04-30 17:16:20 -0400

    controlled form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import Challenge from "./challenges/Challenge5";
-import Header from "./challenges/Header";
-import Main from './challenges/Main'
 import Meme from "./components/Meme";
 import Navbar from "./components/Navbar";
-import Messages from './challenges/Messages';
-import Forms from './challenges/Forms';
-import FormChallenge from './challenges/FormChallenge';
 
 
 function App() {
@@ -14,10 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <FormChallenge />
-      {/* <Messages /> */}
-      {/* <Meme /> */}
-      {/* <Challenge darkMode={true} /> */}
+      <Meme />
     </>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -6,13 +6,15 @@ import Loader from './Loader'
 const inputClasses = "h-9 min-w-0 flex-1 px-2"
 let memes
 
+const initialFormData = {
+  topText: '',
+  bottomText: '',
+  randomImage: null,
+}
+
 const Meme = () => {
   
-  const [meme, setMeme] = useState({
-    topText: '',
-    bottomText: '',
-    randomImage: null,
-  })
+  const [meme, setMeme] = useState(initialFormData)
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
@@ -20,15 +22,25 @@ const Meme = () => {
       .then(res => memes = res.data.memes)
   }, [])
 
-  const handleClick = () => {
+  const handleSubmit = e => {
+    e.preventDefault()
+
     setIsLoading(true)
+
     let random = Math.floor(Math.random()*100)
-    setMeme({
-      topText: '',
-      bottomText: '',
+
+    setMeme(prevState => ({
+      ...prevState,
       randomImage: memes[random].url,
-    })
-    console.log(random)
+    }))
+  }
+
+  const handleInputChange = e => {
+    const {name, value} = e.target
+    setMeme(prevState => ({
+      ...prevState,
+      [name] : value
+    }))
   }
 
   const onImageLoad = () => {
@@ -37,17 +49,28 @@ const Meme = () => {
   
   return (
     <main>
-      <form className="max-w-lg mx-auto py-9">
+      <form
+        className="max-w-lg mx-auto py-9"
+        onSubmit={handleSubmit}
+      >
         <div className="flex gap-5 flex-wrap">
-          <input type="text" className={inputClasses} placeholder="Top text" />
           <input
+            onChange={handleInputChange}
+            name='topText'
+            value={meme.topText}
+            type="text"
+            className={inputClasses}
+            placeholder="Top text"
+          />
+          <input
+            onChange={handleInputChange}
+            name='bottomText'
+            value={meme.bottomText}
             type="text"
             className={inputClasses}
             placeholder="Bottom text"
           />
           <button
-            onClick={handleClick}
-            type="button"
             className="w-full text-white h-9"
           >
             Get a new meme image
